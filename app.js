5444f66e9ca80171c6ef288a0fcd984aad2f596d
MohammadBakhshi <mohammadbakhshi1979@gmail.com>
2022-05-05 10:37:33 +0430

    fix some errors

--- app.js
+++ app.js
@@ -2,16 +2,40 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+const fs = require('fs');
+const { join } = require('path');
 const client = new Client();
 
 
 client.on('qr', (qr) => {
     console.log('QR RECEIVED');
-    qrcode.generate(qr, { small: true });
+    fs.writeFile(join(__dirname, './last_qr'), qr, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    })
+});
+
+client.on("authenticated", () => {
+    console.log("AUTH!");
+    fs.unlink(join(__dirname, './last_qr'), (err) => {
+        if (err) {
+            console.log(err);
+        }
+    })
+});
+
+client.on("auth_failure", () => {
+    console.log("AUTH Failed !");
+    process.exit();
 });
 
 client.on('ready', async () => {
     console.log('Client is ready!');
+});
+
+client.on("disconnected", () => {
+    console.log("disconnected");
 });
 
 
@@ -21,6 +45,19 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+
+app.get('/getqr', (req, res) => {
+    client
+        .getState()
+        .then((data) => {
+            if (data) {
+                res.write("<html><body><h2>Already Authenticated</h2></body></html>");
+                res.end();
+            } else sendQr(res);
+        })
+        .catch(() => sendQr(res));
+})
 
 app.post('/:phone', async (req, res) => {
     const phoneNumber = req.params.phone;
@@ -36,6 +73,40 @@ app.post('/:phone', async (req, res) => {
 
 
 app.listen(3000, () => console.log('Server is running on port 3000...'));
+
+
+function sendQr(res) {
+    fs.readFile(join(__dirname, './last_qr'), (err, last_qr) => {
+        if (!err && last_qr) {
+            var page = `
+                      <html>
+                          <body>
+                              <script type="module">
+                              </script>
+                              <div id="qrcode"></div>
+                              <script type="module">
+                                  import QrCreator from "https://cdn.jsdelivr.net/npm/qr-creator/dist/qr-creator.es6.min.js";
+                                  let container = document.getElementById("qrcode");
+                                  QrCreator.render({
+                                      text: "${last_qr}",
+                                      radius: 0.5, // 0.0 to 0.5
+                                      ecLevel: "H", // L, M, Q, H
+                                      fill: "#536DFE", // foreground color
+                                      background: null, // color or null for transparent
+                                      size: 256, // in pixels
+                                  }, container);
+                              </script>
+                          </body>
+                      </html>
+                  `;
+            res.write(page);
+            res.end();
+        }
+        else if (err) {
+            console.log(err);
+        }
+    });
+}
 
 
 
